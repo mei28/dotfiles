d853aba0779ef68d6610c2d2a551b1c17a23379a
楊明哲 <mingzhe.yang@hcomp.cs.tsukuba.ac.jp>
2020-08-07 00:13:31 +0900

    ADD: vscode json into dotfiles

---
+++ .vscode/vscode_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+
+rm "$VSCODE_SETTING_DIR/settings.json"
+ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json" 
+
+rm "$VSCODE_SETTING_DIR/keybindings.json"
+ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json" 
+
+# install extention
+cat extensions | while read line
+do
+  code --install-extension $line
+done
+
+code --list-extensions > extensions
