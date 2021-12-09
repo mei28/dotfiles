347e46a84122987d5fafa493aa62714afffcdafe
Yang Mingzhe <yang@OSXITsinicxcoms-MacBook-Air.local>
2021-12-09 14:27:49 +0900

    ADD: setups

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ DOT_FILES=(.bashrc  .bash_profile  .config  .git-prompt.sh)
 
 for file in ${DOT_FILES[@]}
 do 
-    ln -sf $HOME/dotfiles/$file $HOME/$file
+    ln -sfv $HOME/dotfiles/$file $HOME/$file
     echo $file
 done
 
