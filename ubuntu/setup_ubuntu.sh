388f17588de3f063d5a6cf9555f3cae8546c3369
mei <mei28aquarius@gmail.com>
2021-12-14 19:23:35 +0900

    ADD: nvidia tool kit

--- ubuntu/setup_ubuntu.sh
+++ ubuntu/setup_ubuntu.sh
@@ -50,3 +50,11 @@ gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
 
 # docker
 sudo apt install docker docker-compose
+
+# nvidia container toolkit
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
