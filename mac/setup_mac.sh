0602c021e1e24a1db8d326bb5d0baa5e85ea0a05
mei <mei28aquarius@gmail.com>
2022-08-11 16:42:34 +0900

    add brew

--- mac/setup_mac.sh
+++ mac/setup_mac.sh
@@ -1,4 +1,6 @@
 brew install bash-completion
 source /usr/local/etc/bash_completion
 
-brew install trash-cli
+brew install tree-sitter
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
