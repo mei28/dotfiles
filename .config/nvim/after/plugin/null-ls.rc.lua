b3ee0bbdd05aac5d8d38517df666ceb8621bd142
mei <mei28aquarius@gmail.com>
2022-08-17 10:49:15 +0900

    fix

--- .config/nvim/after/plugin/null-ls.rc.lua
+++ .config/nvim/after/plugin/null-ls.rc.lua
@@ -1,5 +1,20 @@
 local status, null_ls = pcall(require, 'null-ls')
 if not status then return end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local on_attach = function(client, bufnr)
+    -- you can reuse a shared lspconfig on_attach callback here
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function() vim.lsp.buf.format({bufnr = bufnr}) end
+        })
+    end
+end
+
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.black, null_ls.builtins.diagnostics.flake8,
@@ -7,11 +22,9 @@ null_ls.setup({
 
         null_ls.builtins.diagnostics.luacheck,
         null_ls.builtins.diagnostics.trail_space,
-        null_ls.builtins.formatting.lua_format,
+        null_ls.builtins.formatting.lua_format
 
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.djhtml
-    }
+        -- null_ls.builtins.formatting.prettier, null_ls.builtins.formatting.djhtml
+    },
+    on_attach = on_attach
 })
-
-local bufopts = {noremap = true, silent = true}
