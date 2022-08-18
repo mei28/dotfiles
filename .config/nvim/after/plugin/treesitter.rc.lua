aad682cf1f6b5372882ca70180fdb3469387ab4c
mei <mei28aquarius@gmail.com>
2022-08-18 17:48:35 +0900

    fix

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -2,14 +2,14 @@ local status, ts = pcall(require, 'nvim-treesitter.configs')
 if not status then return end
 
 ts.setup {
-    highlight = {enable = true, disable = {}},
-    indent = {enable = true, disable = {}},
-    ensure_installed = {
-        "tsx", "toml", "php", "json", "yaml", "css", "html", "lua", "python",
-        "cpp"
-    }, autotag = {enable = true},
-    yati = {enable = true}
+  highlight = { enable = true, disable = {} },
+  indent = { enable = true, disable = {'python'} },
+  ensure_installed = {
+    "tsx", "toml", "php", "json", "yaml", "css", "html", "lua", "python",
+    "cpp"
+  }, autotag = { enable = true },
+  yati = { enable = true }
 }
 
-local parser_config = require"nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = {"javascript", "typescript.tsx"}
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,9 +7,11 @@ require 'colorscheme'
 local has = function(x) return vim.fn.has(x) == 1 end
 
 local is_mac = has "macunix"
+local is_ubuntu = has "unix"
 local is_win = has "win32"
 
 if is_mac then require 'macos' end
+if is_ubuntu then require 'macos' end
 if is_win then require 'windows' end
 
 -- auto compile plugins
