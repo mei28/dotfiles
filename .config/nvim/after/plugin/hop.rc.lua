ef4d3e3cbedc75b2eac7196493e67468b41e0e39
mei <mei28aquarius@gmail.com>
2022-09-11 09:53:50 +0900

    add hop

--- .config/nvim/after/plugin/hop.rc.lua
+++ .config/nvim/after/plugin/hop.rc.lua
@@ -2,12 +2,14 @@ local status, hop = pcall(require,'hop')
 if not status then return end
 
 hop.setup({
-  keys = 'etovxqpdygfblzhckisuran'
+  keys = 'asdghklqwertyuiopzxcvbnmfj'
 })
+
 -- place this in one of your configuration file(s)
 vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
 vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
 vim.api.nvim_set_keymap('n', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>", {})
 vim.api.nvim_set_keymap('n', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>", {})
-vim.api.nvim_set_keymap('n', '<leader><leader>f', "<cmd>lua require'hop'.hint_words({hint_offset = 1 })<cr>", {})
-vim.api.nvim_set_keymap('n', '<leader><leader>l', "<cmd>lua require'hop'.hint_lines({hint_offset = 1 })<cr>", {})
+vim.api.nvim_set_keymap('n', '<leader><leader>f', "<cmd>lua require'hop'.hint_char1()<cr>", {})
+vim.api.nvim_set_keymap('n', '<leader><leader>w', "<cmd>lua require'hop'.hint_words()<cr>", {})
+vim.api.nvim_set_keymap('n', '<leader><leader>l', "<cmd>lua require'hop'.hint_lines()<cr>", {})
