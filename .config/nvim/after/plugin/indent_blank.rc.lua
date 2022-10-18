11b7081cc06ae3c234f3ad9df1dede44f75a3ec8
mei <mei28aquarius@gmail.com>
2022-10-18 11:34:10 +0900

    add some

--- .config/nvim/after/plugin/indent_blank.rc.lua
+++ .config/nvim/after/plugin/indent_blank.rc.lua
@@ -1,13 +1,15 @@
 vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "space: "
 -- vim.opt.listchars:append "eol:↴"
 
+
 local status, indent_blankline = pcall(require, 'indent_blankline')
-if not status then 
+if not status then
   return
 end
 indent_blankline.setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_current_context_start = true,
+  show_end_of_line = true,
 }
