e7d35e8b47d00441b290416e15ce6944b69755b0
mei <mei28aquarius@gmail.com>
2022-11-18 18:23:58 +0900

    add tmux like

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,8 +2,113 @@ local status, wezterm = pcall(require, 'wezterm')
 
 if not status then return end
 
+local act = wezterm.action
+local keys = {
 
+  {
+    key = '\\',
+    mods = 'LEADER',
+    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '-',
+    mods = 'LEADER',
+    action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+
+  { key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane { confirm = false } },
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'k',
+    mods = 'LEADER',
+    action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = act.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'H',
+    mods = 'LEADER',
+    action = act.AdjustPaneSize { 'Left', 5 },
+  },
+  {
+    key = 'J',
+    mods = 'LEADER',
+    action = act.AdjustPaneSize { 'Down', 5 },
+  },
+  { key = 'K', mods = 'LEADER', action = act.AdjustPaneSize { 'Up', 5 } },
+  {
+    key = 'L',
+    mods = 'LEADER',
+    action = act.AdjustPaneSize { 'Right', 5 },
+  },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+}
+
+local hyperlink_rules = {
+  -- Linkify things that look like URLs and the host has a TLD name.
+  -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+  {
+    regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
+    format = '$0',
+  },
+
+  -- linkify email addresses
+  -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+  {
+    regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+    format = 'mailto:$0',
+  },
+
+  -- file:// URI
+  -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+  {
+    regex = [[\bfile://\S*\b]],
+    format = '$0',
+  },
+
+  -- Linkify things that look like URLs with numeric addresses as hosts.
+  -- E.g. http://127.0.0.1:8000 for a local development server,
+  -- or http://192.168.1.1 for the web interface of many routers.
+  {
+    regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+    format = '$0',
+  },
+
+  -- Make task numbers clickable
+  -- The first matched regex group is captured in $1.
+  {
+    regex = [[\b[tT](\d+)\b]],
+    format = 'https://example.com/tasks/?t=$1',
+  },
+
+  -- Make username/project paths clickable. This implies paths like the following are for GitHub.
+  -- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+  -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+  -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+  {
+    regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+    format = 'https://www.github.com/$1/$3',
+  },
+}
 return {
+  -- init shell
+  default_prog = { '/bin/bash', '-l' },
   -- font
   font = wezterm.font 'Hack Nerd Font Mono',
   -- color scheme
@@ -11,53 +116,12 @@ return {
   -- turn off beep
   audible_bell = 'Disabled',
   -- completion
-  quick_select_patterns = {"[\\w-]+"},
+  quick_select_patterns = { "\\w-+" },
+  -- like tmux key bind
+  leader = { key = 'q', mods = 'CTRL', timeout_milliseconds = 1000 },
+  -- tmux like key bind
+  keys = keys,
 
   -- hyperlink
-  hyperlink_rules = {
-    -- Linkify things that look like URLs and the host has a TLD name.
-    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
-    {
-      regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
-      format = '$0',
-    },
-
-    -- linkify email addresses
-    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
-    {
-      regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
-      format = 'mailto:$0',
-    },
-
-    -- file:// URI
-    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
-    {
-      regex = [[\bfile://\S*\b]],
-      format = '$0',
-    },
-
-    -- Linkify things that look like URLs with numeric addresses as hosts.
-    -- E.g. http://127.0.0.1:8000 for a local development server,
-    -- or http://192.168.1.1 for the web interface of many routers.
-    {
-      regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-      format = '$0',
-    },
-
-    -- Make task numbers clickable
-    -- The first matched regex group is captured in $1.
-    {
-      regex = [[\b[tT](\d+)\b]],
-      format = 'https://example.com/tasks/?t=$1',
-    },
-
-    -- Make username/project paths clickable. This implies paths like the following are for GitHub.
-    -- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
-    -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
-    -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
-    {
-      regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-      format = 'https://www.github.com/$1/$3',
-    },
-  },
+  hyperlink_rules = hyperlink_rules,
 }
