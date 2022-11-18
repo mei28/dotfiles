eb6818fc55e31a4cd72396db1c9904e0774ebbd6
mei <mei28aquarius@gmail.com>
2022-11-18 10:35:34 +0900

    add some

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,4 +1,4 @@
-DOT_FILES=(.bashrc  .bash_profile  .config  .git-prompt.sh  .gitconfig  .tmux.conf)
+DOT_FILES=(.bashrc  .bash_profile  .config  .git-prompt.sh  .gitconfig  .tmux.conf  .hammerspoon)
 
 for file in ${DOT_FILES[@]}
 do 
@@ -6,6 +6,6 @@ do
     echo $file
 done
 
-# 初回git-promptの認識
-curl -o $HOME/.git-prompt.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-curl -o $HOME/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+# # 初回git-promptの認識
+# curl -o $HOME/.git-prompt.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+# curl -o $HOME/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
