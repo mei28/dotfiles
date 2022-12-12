3905f04a8ac7d70fa2d63a99a12cebb63bd68b04
mei <mei28aquarius@gmail.com>
2022-12-12 22:49:57 +0900

    add

--- .config/nvim/after/plugin/action-preview.rc.lua
+++ .config/nvim/after/plugin/action-preview.rc.lua
@@ -2,7 +2,7 @@ local status, action = pcall(require, 'actions-preview')
 
 if not status then return end
 
-vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
+vim.keymap.set({ "v", "n" }, "ca", require("actions-preview").code_actions)
 action.setup({
 
 })
