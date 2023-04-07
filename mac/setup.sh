cc707d8325454fa63283b1d61d714af392073991
mei <mei28aquarius@gmail.com>
2023-04-07 09:34:41 +0900

    add yabai skhd like i3wm

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,4 +1,4 @@
-DOT_FILES=(.bashrc  .bash_profile  .config  .git-prompt.sh  .gitconfig  .tmux.conf  .hammerspoon .fzf.bash)
+DOT_FILES=(.bashrc  .bash_profile  .config  .git-prompt.sh  .gitconfig  .tmux.conf  .hammerspoon .fzf.bash .yabairc .skhdrc)
 
 for file in ${DOT_FILES[@]}
 do 
