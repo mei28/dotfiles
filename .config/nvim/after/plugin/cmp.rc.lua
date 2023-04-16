fb7cd88306794a0222fb16267e79d8a9d34a518b
mei <mei28aquarius@gmail.com>
2023-04-16 16:05:25 +0900

    Add keybindings for Tab and Shift-Tab to select the next and previous item in the completion menu, respectively, but only if there are words before the cursor, in nvim

--- .config/nvim/after/plugin/cmp.rc.lua
+++ .config/nvim/after/plugin/cmp.rc.lua
@@ -2,6 +2,11 @@ local status, cmp = pcall(require, "cmp")
 if (not status) then return end
 local lspkind = require 'lspkind'
 
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -17,29 +22,29 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({
       select = true
     }),
-    ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
+    ["<Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
       else
         fallback()
       end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
+    end),
+    ["<S-Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
       else
         fallback()
       end
-    end,
+    end),
   }),
   sources = cmp.config.sources({
-    { name = 'copilot' },
     { name = 'buffer' },
     { name = "mocword" },
     { name = 'nvim_lsp' },
     { name = "path" },
     { name = "nvim_lsp_signature_help" },
     { name = "treesitter" },
+    { name = 'copilot' },
     -- { name = 'obsidian' },
     { name = 'tmux' },
   }),
@@ -79,5 +84,7 @@ end)
 cmp.event:on("menu_closed", function()
   vim.b.copilot_suggestion_hidden = false
 end)
+
+
 
 vim.cmd [[highlight! default link CmpItemKind CmpItemMenuDefault]]
