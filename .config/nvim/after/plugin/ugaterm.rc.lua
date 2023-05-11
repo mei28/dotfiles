33ae29419b166ba2f66c0069d51fa172a0a388d1
mei <mei28aquarius@gmail.com>
2023-05-11 15:26:10 +0900

    Add key mappings for toggling the ugaterm plugin in normal mode using <C-`> and <C-t>.

--- .config/nvim/after/plugin/ugaterm.rc.lua
+++ .config/nvim/after/plugin/ugaterm.rc.lua
@@ -1,6 +1,8 @@
 local status, uterm = pcall(require, "ugaterm")
 if not status then return end
+local set = vim.keymap.set
 
 uterm.setup {}
 
-vim.keymap.set('n', '<C-`>', "<CMD>UgatermToggle<CR>")
+set({ 'n', 't' }, '<C-`>', "<CMD>UgatermToggle<CR>")
+set({ 'n', 't' }, '<C-t>', "<CMD>UgatermToggle<CR>")
