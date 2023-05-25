0f10f13773b8fb277fe876dd19dca31b2ee283af
mei <mei28aquarius@gmail.com>
2023-05-25 18:33:11 +0900

    Add lsp_names function to display active LSP clients and their sources in lualine

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -3,6 +3,22 @@ if not status then
   return
 end
 
+local lsp_names = function()
+  local clients = {}
+  for _, client in ipairs(vim.lsp.get_active_clients { bufnr = 0 }) do
+    if client.name == 'null-ls' then
+      local sources = {}
+      for _, source in ipairs(require('null-ls.sources').get_available(vim.bo.filetype)) do
+        table.insert(sources, source.name)
+      end
+      table.insert(clients, 'null-ls(' .. table.concat(sources, ', ') .. ')')
+    else
+      table.insert(clients, client.name)
+    end
+  end
+  return ' ' .. table.concat(clients, '/')
+end
+
 lualine.setup {
   options = {
     icons_enabled = true,
@@ -13,7 +29,7 @@ lualine.setup {
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff' },
+    lualine_b = { 'diff' },
     lualine_c = {
       {
         'filename',
@@ -26,16 +42,28 @@ lualine.setup {
           unnamed = 'Untitled',
         }
       }
-    }, lualine_x = {
-      { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
-        hint = ' ' } },
-      'encoding',
-      'filetype'
+    },
+    lualine_x = {
+      {
+        'diagnostics',
+        sources = { "nvim_diagnostic" },
+        symbols = {
+          error = ' ',
+          warn = ' ',
+          info = ' ',
+          hint = ' '
+        }
+      },
     },
     -- lualine_y = { 'progress' },
     -- lualine_z = { 'location' }
-    lualine_y = { '' },
-    lualine_z = { '' }
+    lualine_y = {
+      'filetype',
+      'encoding',
+    },
+    lualine_z = {
+      lsp_names
+    },
   },
   inactive_sections = {
     lualine_a = {},
@@ -43,7 +71,7 @@ lualine.setup {
     lualine_c = { {
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
-      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      path = 1            -- 0 = just filename, 1 = relative path, 2 = absolute path
     } },
     lualine_x = { 'location' },
     lualine_y = {},
