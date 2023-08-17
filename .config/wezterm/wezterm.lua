4f5c7477bbd9b02abcdb668bac3ee8e04393b485
mei <mei28aquarius@gmail.com>
2023-08-17 11:37:12 +0900

    add

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -197,7 +197,7 @@ config.skip_close_confirmation_for_processes_named = skip_close_confirmation_for
 config.inactive_pane_hsb = inactive_pane_hsb
 config.check_for_updates = false
 -- hide title bar
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE|TITLE"
 -- background image
 config.window_background_image = utils:randomBackgroundImage()
 config.window_background_image_hsb = {
