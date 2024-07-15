90b2b75f354d515087efdd909d0f282e6b98724f
mei <mei28aquarius@gmail.com>
2024-07-15 10:01:01 +0900

    update

--- .config/nvim/lua/plugin/hlslens.lua
+++ .config/nvim/lua/plugin/hlslens.lua
@@ -8,14 +8,26 @@ local spec = {
       hlslens.setup()
       require("scrollbar.handlers.search").setup({})
     end,
+    dependencies = { 'rapan931/lasterisk.nvim' },
     keys = {
-      { 'n',  [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]] },
-      { 'N',  [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]] },
-      { '*',  [[*<Cmd>lua require('hlslens').start()<CR>]] },
+      { 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]] },
+      { 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]] },
+      -- { '*',  [[*<Cmd>lua require('hlslens').start()<CR>]] },
+      { '*', function()
+        require("lasterisk").search()
+        require('hlslens').start()
+      end },
       { '#',  [[#<Cmd>lua require('hlslens').start()<CR>]] },
-      { 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]] },
+      -- { 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]] },
+      { 'g*', function()
+        require("lasterisk").search({ is_whole = false })
+        require('hlslens').start()
+      end },
       { 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]] },
     }
+  },
+  {
+    'rapan931/lasterisk.nvim'
   }
 
 }
