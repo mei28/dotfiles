7342f1bac7ad4cfb62784980ccce51ed213f015e
mei <mei28aquarius@gmail.com>
2024-07-23 10:22:12 +0900

    update

--- .config/nvim/lua/plugin/dmacro.lua
+++ .config/nvim/lua/plugin/dmacro.lua
@@ -1,11 +1,12 @@
 local spec = {
   {
-    'tani/dmacro.nvim',
+    'tani/dmacro.vim',
     event = { 'CursorHold', 'CursorMoved' },
     config = function()
-      require('dmacro').setup({
-        dmacro_key = '<C-2>' --  you need to set the dmacro_key
-      })
+      -- require('dmacro').setup({
+      --   dmacro_key = '<C-2>' --  you need to set the dmacro_key
+      -- })
+      vim.keymap.set('n', '<C-2>', '<Plug>(dmacro_play_macro)')
     end,
   }
 }
