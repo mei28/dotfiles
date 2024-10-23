3e32c1bee9c5a02ddcdc5fa069fc4b4e984a7282
mei <mei28aquarius@gmail.com>
2024-10-23 09:52:16 +0900

    update

---
+++ .config/nix/home-manager/home.nix
@@ -0,0 +1,83 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  username = "mei";
+in {
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+    overlays = [
+      inputs.neovim-nightly-overlay.overlays.default
+    ];
+  };
+
+  home = {
+    username = username;
+    homeDirectory = "/Users/${username}";
+
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "24.05";
+
+    packages = with pkgs; [
+        git
+        curl
+        alejandra
+        gitui
+        wezterm
+        arxiv-latex-cleaner
+        bash
+        bash-completion
+        bat
+        coreutils
+        csvlens
+        deno
+        efm-langserver
+        fastfetch
+        fd
+        ffmpeg
+        ffmpegthumbnailer
+        fzf
+        gh
+        ghostscript
+        gitui
+        heroku
+        imagemagick
+        jujutsu
+        jq
+        libevent
+        libgit2
+        libheif
+        libssh2
+        llvm
+        luajit
+        luarocks
+        neovim
+        nodejs_20
+        openssl
+        ripgrep
+        rust-analyzer
+        serie
+        unzip
+        tig
+        tldr
+        tmux
+        tmux-mem-cpu-load
+        tokei
+        trash-cli
+        tree
+        tree-sitter
+        unbound
+        wget
+        yazi
+        zoxide
+        nerdfonts
+    ];
+  };
+
+  programs.home-manager.enable = true;
+}
--- flake.nix
+++ flake.nix
@@ -4,73 +4,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    neovim-nightly-overlay,
-  }: let
+    home-manager,
+    ...
+  } @ inputs: let
     system = "aarch64-darwin";
-    pkgs = nixpkgs.legacyPackages.${system}.extend (
-      neovim-nightly-overlay.overlays.default
-    );
+    pkgs = import nixpkgs {inherit system;};
   in {
     packages.${system}.my-packages = pkgs.buildEnv {
       name = "my-packages-list";
-      paths = with pkgs; [
-        git
-        curl
-        alejandra
-        gitui
-        wezterm
-        arxiv-latex-cleaner
-        bash
-        bash-completion
-        bat
-        coreutils
-        csvlens
-        deno
-        efm-langserver
-        fastfetch
-        fd
-        ffmpeg
-        ffmpegthumbnailer
-        fzf
-        gh
-        ghostscript
-        gitui
-        heroku
-        imagemagick
-        jj
-        jq
-        libevent
-        libgit2
-        libheif
-        libssh2
-        llvm
-        luajit
-        luarocks
-        neovim
-        nodejs_20
-        openssl
-        ripgrep
-        rust-analyzer
-        serie
-        unzip
-        tig
-        tldr
-        tmux
-        tmux-mem-cpu-load
-        tokei
-        trash-cli
-        tree
-        tree-sitter
-        unbound
-        wget
-        yazi
-        zoxide
-      ];
+      paths = with pkgs; [];
     };
 
     # nix run .#update
@@ -81,9 +32,20 @@
         echo "Updating flake..."
         nix flake update
         echo "Updating profile..."
-        nix profile upgrade my-packages
-        echo "Update complete!"
+        echo "Updating home-manager..."
+        nix run nixpkgs#home-manager -- switch --flake .#myHomeConfig
       '');
+    };
+    homeConfigurations = {
+      myHomeConfig = home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgs;
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./.config/nix/home-manager/home.nix
+        ];
+      };
     };
   };
 }
