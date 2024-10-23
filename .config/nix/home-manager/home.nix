c03f49bea98c27f258ef87d7db1142b1227a6c25
mei <mei28aquarius@gmail.com>
2024-10-23 14:21:20 +0900

    update

--- .config/nix/home-manager/home.nix
+++ .config/nix/home-manager/home.nix
@@ -24,61 +24,64 @@ in {
     stateVersion = "24.05";
 
     packages = with pkgs; [
-        git
-        curl
-        alejandra
-        gitui
-        wezterm
-        arxiv-latex-cleaner
-        bash
-        bash-completion
-        bat
-        coreutils
-        csvlens
-        deno
-        efm-langserver
-        fastfetch
-        fd
-        ffmpeg
-        ffmpegthumbnailer
-        fzf
-        gh
-        ghostscript
-        gitui
-        heroku
-        imagemagick
-        jujutsu
-        jq
-        sqlite
-        libevent
-        libgit2
-        libheif
-        libssh2
-        llvm
-        luajit
-        luarocks
-        neovim
-        nodejs_20
-        openssl
-        ripgrep
-        rust-analyzer
-        serie
-        unzip
-        tig
-        tldr
-        tmux
-        tmux-mem-cpu-load
-        tokei
-        trash-cli
-        tree
-        tree-sitter
-        unbound
-        wget
-        yazi
-        zoxide
-        nerdfonts
+      git
+      curl
+      alejandra
+      gitui
+      wezterm
+      arxiv-latex-cleaner
+      bash
+      bash-completion
+      bat
+      coreutils
+      csvlens
+      deno
+      efm-langserver
+      fastfetch
+      fd
+      ffmpeg
+      ffmpegthumbnailer
+      fzf
+      gh
+      ghostscript
+      gitui
+      heroku
+      imagemagick
+      jujutsu
+      jq
+      sqlite
+      libevent
+      libgit2
+      libheif
+      libssh2
+      llvm
+      luajit
+      luarocks
+      neovim
+      nodejs_20
+      openssl
+      ripgrep
+      rust-analyzer
+      serie
+      unzip
+      tig
+      tldr
+      tmux
+      tmux-mem-cpu-load
+      tokei
+      trash-cli
+      tree
+      tree-sitter
+      unbound
+      wget
+      yazi
+      zoxide
+      nerdfonts
     ];
-  };
 
-  programs.home-manager.enable = true;
+    # Home Manager programs configuration
+    programs.home-manager.enable = true;
+
+  };
 }
+
