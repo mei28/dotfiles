4ab7a6add0514577e361486626cdd48813f99ad5
mei <mei28aquarius@gmail.com>
2024-10-25 16:26:39 +0900

    update

--- .config/wezterm/window.lua
+++ .config/wezterm/window.lua
@@ -40,4 +40,24 @@ M.toggle_opacity = function(window, pane)
   window:set_config_overrides(overrides)
 end
 
+
+-- https://zenn.dev/mozumasu/articles/mozumasu-wezterm-customization
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#5c6d74"
+  local foreground = "#FFFFFF"
+
+  if tab.is_active then
+    background = "#ae8b2d"
+    foreground = "#FFFFFF"
+  end
+
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+  return {
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+  }
+end)
+
 return M
