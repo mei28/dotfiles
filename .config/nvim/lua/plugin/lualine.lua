3ab962767d32b46cac8384ea202cac47a4b2ae0f
mei <mei28aquarius@gmail.com>
2024-10-25 16:40:10 +0900

    update

--- .config/nvim/lua/plugin/lualine.lua
+++ .config/nvim/lua/plugin/lualine.lua
@@ -42,8 +42,10 @@ function lualine_setup()
   lualine.setup {
     options = {
       icons_enabled = true,
-      section_separators = { left = '', right = '' },
-      component_separators = { left = '', right = '' },
+      -- section_separators = { left = '', right = '' },
+      -- component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' },
       disabled_filetypes = {}
     },
     sections = {
