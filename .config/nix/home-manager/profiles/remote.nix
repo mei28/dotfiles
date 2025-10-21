7c16e68579bea91767678f38fc579506ad65e5bb
mei <mei28aquarius@gmail.com>
2025-10-21 14:09:21 +0900

    upd

--- .config/nix/home-manager/profiles/remote.nix
+++ .config/nix/home-manager/profiles/remote.nix
@@ -40,8 +40,8 @@ in
 
   # Git設定（リモート環境用）
   # 既存のmodules/git.nixがあるため、上書きが必要な場合のみ
-  programs.git = {
-    userName = lib.mkDefault username;
-    userEmail = lib.mkDefault "${username}@remote.local";
+  programs.git.settings = {
+    user.name = lib.mkDefault username;
+    user.email = lib.mkDefault "${username}@remote.local";
   };
 }
