f3eae0b3f6a7def5777e972fcdbff033fb709cc9
mei <51149822+mei28@users.noreply.github.com>
2025-12-30 21:37:58 +0900

    up

--- .config/nvim/lua/plugin/scrollbar.lua
+++ .config/nvim/lua/plugin/scrollbar.lua
@@ -14,38 +14,15 @@ local spec = {
 			scrolloff_percentage = 0.2,
 		},
 	},
-	-- {
-	--   'wsdjeg/scrollbar.vim',
-	--   event = { "CursorMoved", "CursorHold" },
-	--   config = function()
-	--     require('scrollbar').setup({
-	--       max_size = 10,
-	--       min_size = 5,
-	--       width = 1,
-	--       right_offset = 1,
-	--       excluded_filetypes = {
-	--         'startify',
-	--         'git-commit',
-	--         'leaderf',
-	--         'NvimTree',
-	--         'tagbar',
-	--         'defx',
-	--         'neo-tree',
-	--         'qf',
-	--       },
-	--       shape = {
-	--         head = '',
-	--         body = '█',
-	--         tail = '',
-	--       },
-	--       highlight = {
-	--         head = 'Normal',
-	--         body = 'Normal',
-	--         tail = 'Normal',
-	--       },
-	--     })
-	--   end,
-	-- },
+	{
+		"petertriho/nvim-scrollbar",
+		event = { "CursorMoved", "CursorHold" },
+		config = function()
+			require("scrollbar").setup()
+			require("scrollbar.handlers.search").setup()
+			require("scrollbar.handlers.gitsigns").setup()
+		end,
+	},
 }
 
 return spec
