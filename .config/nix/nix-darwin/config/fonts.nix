13d40e3addc5888c2da6808996cd9314164cbb44
mei <51149822+mei28@users.noreply.github.com>
2026-05-06 20:11:45 +0900

    refactor(nix-darwin): move nerd-fonts to nix, drop wezterm stable

--- .config/nix/nix-darwin/config/fonts.nix
+++ .config/nix/nix-darwin/config/fonts.nix
@@ -2,7 +2,8 @@
   fonts.packages = with pkgs; [
     hackgen-font
     hackgen-nf-font
-    # nerd-fonts
+    nerd-fonts.hack
+    nerd-fonts.symbols-only
     font-awesome
     font-awesome_5
   ];
