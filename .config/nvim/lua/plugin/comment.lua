2255c6e6f97ec16708defc29c41d418b7b7777a0
mei <51149822+mei28@users.noreply.github.com>
2026-05-14 11:29:37 +0900

    upd

--- .config/nvim/lua/plugin/comment.lua
+++ .config/nvim/lua/plugin/comment.lua
@@ -1,43 +1,45 @@
 local spec = {
 
-  -- comment
-  {
-    'numToStr/Comment.nvim',
-    config = function() comment_setup() end,
-    keys = {
-      { 'gcl', 'gbc' }
-    },
-    event = "ModeChanged",
-    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' }
-  },
-  {
-    "folke/ts-comments.nvim",
-    keys = {
-      { 'gcl', 'gbc' }
-    },
-    enabled = vim.fn.has("nvim-0.10.0") == 1,
-  },
+	-- comment
+	{
+		"nvim-mini/mini.comment",
+	},
 
+	-- {
+	-- 	"numToStr/Comment.nvim",
+	-- 	config = function()
+	-- 		comment_setup()
+	-- 	end,
+	-- 	keys = {
+	-- 		{ "gcl", "gbc" },
+	-- 	},
+	-- 	-- event = "ModeChanged",
+	-- 	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+	-- },
+	{
+		"folke/ts-comments.nvim",
+		keys = {
+			{ "gcl", "gbc" },
+		},
+	},
 }
 
 function comment_setup()
-  local status, comment = pcall(require, 'Comment')
-  if not status then
-    return
-  end
+	local status, comment = pcall(require, "Comment")
+	if not status then
+		return
+	end
 
-  comment.setup(
-    {
-      ---LHS of toggle mappings in NORMAL mode
-      ---@type table
-      toggler = {
-        ---Line-comment toggle keymap
-        line = 'gcl',
-        ---Block-comment toggle keymap
-        block = 'gbc',
-      },
-    }
-  )
+	comment.setup({
+		---LHS of toggle mappings in NORMAL mode
+		---@type table
+		toggler = {
+			---Line-comment toggle keymap
+			line = "gcl",
+			---Block-comment toggle keymap
+			block = "gbc",
+		},
+	})
 end
 
 return spec
--- .config/nvim/lua/plugin/dropbar.lua
+++ .config/nvim/lua/plugin/dropbar.lua
@@ -2,6 +2,7 @@ local spec = {
   {
     'Bekaboo/dropbar.nvim',
     event = { 'BufNewFile', 'BufRead' },
+    enabled = false, -- nvim 0.13 で BufModifiedSet deprecated。https://github.com/Bekaboo/dropbar.nvim/pull/280 マージ待ち
   },
 
 }
