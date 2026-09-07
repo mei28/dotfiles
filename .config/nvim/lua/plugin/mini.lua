542dc55e5c7d3788ad0b7e774fd527b960fb6add
mei <51149822+mei28@users.noreply.github.com>
2026-09-07 16:56:29 +0900

    style(nvim): format mini.lua with stylua defaults
    Tabs and double quotes, matching stylua's defaults since the repo ships no
    stylua.toml. No behaviour change.

--- .config/nvim/lua/plugin/mini.lua
+++ .config/nvim/lua/plugin/mini.lua
@@ -1,22 +1,21 @@
 local spec = {
-  {
-    'echasnovski/mini.align',
-    version = false,
-    event   = { 'CursorMoved', 'ModeChanged' },
-    config  = function()
-      require('mini.align').setup({})
-    end
-  },
-  {
-    'echasnovski/mini.diff',
-    version = false,
-    event = { 'CursorMoved', "ModeChanged" },
-    config = function()
-      require('mini.diff').setup({
-      })
-    end,
-    keys = { { 'gh' }, { 'gH' }, { '[H' }, { '[h' }, { ']H' }, { ']h' } }
-  }
+	{
+		"echasnovski/mini.align",
+		version = false,
+		event = { "CursorMoved", "ModeChanged" },
+		config = function()
+			require("mini.align").setup({})
+		end,
+	},
+	{
+		"echasnovski/mini.diff",
+		version = false,
+		event = { "CursorMoved", "ModeChanged" },
+		config = function()
+			require("mini.diff").setup({})
+		end,
+		keys = { { "gh" }, { "gH" }, { "[H" }, { "[h" }, { "]H" }, { "]h" } },
+	},
 }
 
 return spec
